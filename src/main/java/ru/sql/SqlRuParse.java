9fd516c9f6d5047798fb5cf54ae40575cda78b93
KirillBelyaev74 <kirbel74@gmail.com>
2020-10-28 12:22:59 +0500

    2. Парсинг HTML страницы.[260358#263179]

---
+++ src/main/java/ru/sql/SqlRuParse.java
@@ -0,0 +1,20 @@
+package ru.sql;
+
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.*;
+import org.jsoup.select.Elements;
+
+public class SqlRuParse {
+    public static void main(String[] args) throws Exception {
+        Document document = Jsoup.connect("https://www.sql.ru/forum/job-offers").get();
+        Elements elements = document.select(".postslisttopic");
+        Elements elementsDate = document.select(".altCol");
+        for (Element element : elements) {
+            System.out.println(element.child(0).attr("href"));
+            System.out.println(element.child(0).text());
+        }
+        for (Element element : elementsDate) {
+            System.out.println(element.childNodes().get(0));
+        }
+    }
+}
